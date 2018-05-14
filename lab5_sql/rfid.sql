e21064fe16efce571789785c94f967e2ec99521f
fall0death <503703217@qq.com>
2018-05-14 11:29:07 +0800

    删掉无关文件，建立card表

--- lab5_sql/rfid.sql
+++ lab5_sql/rfid.sql
@@ -1,3 +1,10 @@
 drop database if exists `rfid`;
 create database `rfid` character set utf8;
 use `rfid`;
+
+CREATE TABLE `card` (
+`money`  varchar(255) NOT NULL ,
+`uid`  varchar(8) NOT NULL ,
+PRIMARY KEY (`uid`)
+);
+
